b8f92eba6d4a9e426a91414c48171768980db78b
Bruno Silva <bsilva@ua.pt>
2017-11-26 23:59:44 +0000

    correçoes

--- app/src/main/java/com/icm/projeto/vitalpaint/CreateGameFragment.java
+++ app/src/main/java/com/icm/projeto/vitalpaint/CreateGameFragment.java
@@ -222,7 +222,7 @@ public class CreateGameFragment extends Fragment {
         }
     }
 
-    
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
